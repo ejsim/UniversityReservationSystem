220b094b52f220268596e2159381f6c531c7c3a8
Max Greenwald <maxgreenwald@me.com>
2017-04-04 03:23:35 -0400

    changed initial page

--- config.py
+++ config.py
@@ -19,7 +19,7 @@ if os.path.exists('config.env'):
 
 
 class Config:
-    APP_NAME = 'URS'
+    APP_NAME = 'University Reservation System'
     if os.environ.get('SECRET_KEY'):
         SECRET_KEY = os.environ.get('SECRET_KEY')
     else:
